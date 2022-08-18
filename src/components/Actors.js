4d4b01baa72c538db4077655a97d01c5ebda4d6d
Tanner <troxs690@gmail.com>
2022-08-18 17:40:26 -0500

    DONE

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,7 +2,21 @@ import React from "react";
 import { actors } from "../data";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+      <h1>Actors Page</h1>
+      {actors.map(actor => (
+        <div key={actor.name}>
+          <h1>{actor.name}</h1>
+          <ul>
+            {actor.movies.map(act => (
+              <li key={act}>{act}</li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default Actors;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,23 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+      <h1>Movies Page</h1>
+      {movies.map(movie => (
+        <div key={movie.title}>
+          <h1>Name: {movie.title}</h1>
+          <h3>Time: {movie.time}</h3>
+          <h3>Genres:</h3>
+          <ul>
+            {movie.genres.map(genre => (
+              <li key={genre}>{genre}</li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default Movies;
